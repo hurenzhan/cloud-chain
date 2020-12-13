6d38165904786b4e16a575f80588825ec7a525ae
hurenren <hurenfa@gmail.com>
2020-12-14 07:52:13 +0800

    提交

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: ["@vue/app"],
   plugins: [
     [
       "import",
-      {
-        libraryName: "ant-design-vue",
-        libraryDirectory: "es",
-        style: true
-      }
+      { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
     ]
   ]
 };
