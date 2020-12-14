1134735e875f714afe2194656e95c94414bc3d82
hurenren <hurenfa@gmail.com>
2020-12-15 00:16:30 +0800

    提交

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -3,14 +3,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from 'vue';
-import mapStore from '@/libs/mapStore';
+import { defineComponent, reactive, toRefs } from "vue";
+import mapStore from "@/libs/mapStore";
 export default defineComponent({
+  name: "login",
   setup() {
-    const loginStore = mapStore('login');
+    const loginStore = mapStore("login");
     const { getState, getActions } = loginStore;
-    const { handleLogin } = getActions(['handleLogin']);
-    const { loading } = getState(['loading']);
+    const { handleLogin } = getActions(["handleLogin"]);
+    const { loading } = getState(["loading"]);
 
     const state = reactive({});
     return {
