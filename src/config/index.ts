250433989c781f279171e2c42c669acacd2ed261
hurenren <hurenfa@gmail.com>
2020-12-17 00:23:12 +0800

    提交

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,7 +14,7 @@ export default {
   /**
    * @description 默认打开的首页的路由name值，默认为home
    */
-  homeName: 'documents',
+  homeName: 'home',
   /**
    * @description 需要加载的插件
    */
--- src/router/modules/login.ts
+++ src/router/modules/login.ts
@@ -1,20 +1,26 @@
-import { getToken } from '@/libs/utils'
+// import { getToken } from '@/libs/utils'
 import { Component } from 'vue'
 import { RouteRecordRaw } from 'vue-router'
 const Login: Component = () => import(/* webpackChunkName: "about" */ '@/views/login/Login.vue')
+const Create: Component = () => import(/* webpackChunkName: "about" */ '@/views/login/Create.vue')
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/login',
     name: 'login',
     component: Login,
-    beforeEnter: (to, { fullPath }, next) => {
-      const token = getToken()
-      if (token) {
-        return next(fullPath)
-      }
-      next()
-    }
+    // beforeEnter: (to, { fullPath }, next) => {
+    //   const token = getToken()
+    //   if (token) {
+    //     return next(fullPath)
+    //   }
+    //   next()
+    // }
+  },
+  {
+    path: '/createEnterprise',
+    name: 'createEnterprise',
+    component: Create,
   },
 ]
 
